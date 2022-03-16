b63d7ccbc64cf296445a5f91ac18bcd620f4a434
Jennifer <jenniferroques@gmail.com>
2022-03-16 11:35:04 -0500

    Updated Search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,19 @@
 import React from "react";
 
+function Search({handleSearch}) {
 
-
-
+    return (
+        <div className="searchbar">
+            <label htmlFor="search">Search Constellations:</label>
+            <input 
+                type="text"
+                id="search"
+                placeholder="Type name to search..."
+                onChange={handleSearch}
+            />
+        </div>
+    );
+}
 
 
 
